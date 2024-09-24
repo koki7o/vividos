e768f6c3fa5b0a0b8761111df9900ecfbf18b249
koki7o <koki7o@users.noreply.github.com>
2024-09-24 22:05:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install screen dnf gimp blender inkscape kdenlive audacity krita obs-studio darktable shotcut openshot
 
 # this would install a package from rpmfusion
-# rpm-ostree install vlc
+rpm-ostree install vlc 
 
 #### Example for enabling a System Unit File
 
